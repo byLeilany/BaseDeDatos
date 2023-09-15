dd021ed2a38729abba6a7098375a0590a5874066
Daniela Meléndez <byleilany@gmail.com>
2023-09-15 16:45:21 -0300

    olas

--- SQLite(8).sql
+++ SQLite(8).sql
@@ -31,6 +31,7 @@ FROM (SELECT k.ArtistId, k.Name, COUNT(*) as 'songsEnPlaylist'
  --2.4) a) quiero playlist que NO tengan NIGNUN track de los artistas X e Y.
  -- no lo hice en AR ni en CRT
 
+/*
 SELECT * FROM Playlist p
 EXCEPT
 SELECT DISTINCT pt.PlaylistId, p.Name
@@ -40,14 +41,36 @@ SELECT DISTINCT pt.PlaylistId, p.Name
  	INNER JOIN Playlist p ON p.PlaylistId  = pt.PlaylistId
     INNER JOIN Artist a ON a.ArtistId = al.ArtistId
     WHERE a.Name IN ('Black Sabbath','Chico Buarque')
- 
--- b) quiero ctes que compraron tracks de 1 UNICO genero
-SELECT * 
-	FROM Customer c
-    INNER JOIN Invoice i ON i.CustomerId = c.CustomerId
-    INNER JOIN InvoiceLine il ON il.InvoiceId = i.InvoiceId
-    
+*/
 
+-- b) quiero ctes que compraron tracks de 1 UNICO genero
+/*
+SELECT k.CustomerId, k.FirstName, k.LastName, COUNT(*)
+    FROM (SELECT DISTINCT cteLinea.CustomerId, cteLinea.FirstName, cteLinea.LastName, g.GenreId
+            FROM (SELECT c.CustomerId, c.FirstName, c.LastName, il.TrackId
+                  FROM Customer c
+                  INNER JOIN Invoice i ON i.CustomerId = c.CustomerId
+                  INNER JOIN InvoiceLine il ON il.InvoiceId = i.InvoiceId) cteLinea
+            INNER join Track t ON t.TrackId = cteLinea.TrackId
+            INNER join Genre g ON t.GenreId = g.GenreId) k
+	GROUP BY k.CustomerId, k.FirstName, k.LastName
+    HAVING COUNT(*) <5
+*/
+
+--INSERT INTO [Invoice] ([InvoiceId], [CustomerId], [InvoiceDate], [BillingAddress], [BillingCity], [BillingCountry], [BillingPostalCode], [Total]) VALUES (412, 58, '2013-12-22 00:00:00', '12,Community Centre', 'Delhi', 'India', '110017', 1.99);
+
+
+
+SELECT DISTINCT cteLinea.CustomerId, cteLinea.FirstName, cteLinea.LastName, t.Name, cteLinea.InvoiceId, cteLinea.InvoiceLineId, g.GenreId
+            FROM (SELECT c.CustomerId, c.FirstName, c.LastName, il.TrackId, i.InvoiceId, il.InvoiceLineId
+                  FROM Customer c
+                  INNER JOIN Invoice i ON i.CustomerId = c.CustomerId
+                  INNER JOIN InvoiceLine il ON il.InvoiceId = i.InvoiceId) cteLinea
+            INNER join Track t ON t.TrackId = cteLinea.TrackId
+            INNER join Genre g ON t.GenreId = g.GenreId
+ORDER BY cteLinea.CustomerId
+/*
+ */
  
  
  
