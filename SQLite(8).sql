e25950ed6a3ee6c63d6191053df64abb26a52f0a
Daniela Meléndez <byleilany@gmail.com>
2023-09-21 13:02:50 -0300

    olap

--- SQLite(8).sql
+++ SQLite(8).sql
@@ -281,6 +281,163 @@ SELECT g.idGenero, g.nombreGenero
     WHERE a.edad = (SELECT MIN (a.edad)	FROM Actor a)
 */
 
+-- 2.6) 
+-- PARTIDO (equipo1, equipo2, torneo, goles1, goles2, puntos1, puntos2)
+-- JUGADOR (nombre, edad, equipo)
+
+--i. Listar los equipos que hayan finalizado invictos (no perdieron ningun partido) en al menos dos torneos.
+/*
+SELECT p.equipo1
+FROM (SELECT k.equipo1, COUNT(*) as '#TorneosInvictos'
+        FROM (SELECT p.equipo1, p.torneo
+                  FROM PARTIDO p 
+                  WHERE p.equipo1 NOT IN (SELECT DISTINCT k.equipo2 FROM PARTIDO p WHERE p.goles1 > p.goles2) AND
+                        p.equipo1 NOT IN (SELECT DISTINCT k.equipo1 FROM PARTIDO p WHERE p.goles1 < p.goles2)) k
+        GROUP BY k.equipo1
+        HAVING COUNT(*) >= 2) k
+*/
+-- ii. Listar los torneos que finalizaron con mas de un candidato a campeon.
+/*
+Los candidatos a campeones de un torneo se determinan sumando los puntos obtenidos
+por cada equipo, seleccionando los que suman el maximo valor.
+*/
+
+/*
+SELECT m.equipo
+FROM (SELECT k.equipo, k.torneo, SUM(k.puntos) as 'puntajes'
+      FROM (SELECT p.equipo1 as 'equipo', p.idTorneo as 'torneo', p.puntos1 as 'puntos'
+            from PARTIDO p
+            UNION ALL
+            SELECT p.equipo2 as 'equipo', p.idTorneo as 'torneo', p.puntos2 as 'puntos'
+            from PARTIDO p
+            ) k
+      GROUP BY k.equipo, k.torneo) m
+WHERE m.torneo, m.puntjaes IN (SELECT q.torneo, MAX(puntajes)
+                                FROM (SELECT k.equipo, k.torneo, SUM(k.puntos) as 'puntajes'
+                                      FROM (SELECT p.equipo1 as 'equipo', p.idTorneo as 'torneo', p.puntos1 as 'puntos'
+                                            from PARTIDO p
+                                            UNION ALL
+                                            SELECT p.equipo2 as 'equipo', p.idTorneo as 'torneo', p.puntos2 as 'puntos'
+                                            from PARTIDO p
+                                            ) k
+                                      GROUP BY k.equipo, k.torneo) q
+                                GROUP By q.torneo)
+*/
+
+
+-- PARTIDO (equipo1, equipo2, torneo, goles1, goles2, puntos1, puntos2)
+/*
+CREATE TABLE [PARTIDO]
+(
+  	[equipo1] NVARCHAR(160)  NOT NULL,
+  	[equipo2] NVARCHAR(160)  NOT NULL,
+    [idTorneo] INTEGER  NOT NULL,    
+    [puntos1] INTEGER  NOT NULL,
+  	[puntos2] INTEGER  NOT NULL,
+    CONSTRAINT [PK_PARTIDO] PRIMARY KEY  ([equipo1], [equipo2], [idTorneo])
+);
+
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','River', 1, 3,0);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','Huracan', 1, 1,1);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','Velez', 1, 3,0);
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('River','Huracan', 1, 3,0);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('River','Velez', 1, 1,1);
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Velez','Huracan', 1, 1,1);
+
+
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','River', 2, 1,1);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','Huracan', 2, 1,1);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Boca','Velez', 2, 1,1);
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('River','Huracan', 2, 1,1);
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('River','Velez', 2, 1,1);
+
+INSERT INTO [PARTIDO] ([equipo1], [equipo2], [idTorneo], [puntos1], [puntos2]) VALUES ('Velez','Huracan', 2, 1,1);
+                                      
+                                      
+*/
+
+/*
+SELECT m.torneo, COUNT(m.equipo) as '+de1Campeon'
+FROM (SELECT k.equipo, k.torneo, SUM(k.puntos) as 'puntajes'
+      FROM (SELECT p.equipo1 as 'equipo', p.idTorneo as 'torneo', p.puntos1 as 'puntos'
+            from PARTIDO p
+            UNION ALL
+            SELECT p.equipo2 as 'equipo', p.idTorneo as 'torneo', p.puntos2 as 'puntos'
+            from PARTIDO p
+            ) k
+      GROUP BY k.equipo, k.torneo) m
+WHERE (m.torneo, m.puntajes) IN (SELECT q.torneo, MAX(puntajes)
+                                FROM (SELECT k.equipo, k.torneo, SUM(k.puntos) as 'puntajes'
+                                      FROM (SELECT p.equipo1 as 'equipo', p.idTorneo as 'torneo', p.puntos1 as 'puntos'
+                                            from PARTIDO p
+                                            UNION ALL
+                                            SELECT p.equipo2 as 'equipo', p.idTorneo as 'torneo', p.puntos2 as 'puntos'
+                                            from PARTIDO p
+                                            ) k
+                                      GROUP BY k.equipo, k.torneo) q
+                                GROUP By q.torneo)
+GROUP BY m.torneo
+HAVING COUNT(m.equipo) >= 2
+*/
+
+-- PERSONA(Nombre, Genero, Fecha Nac, Nombre Madre, Nombre Padre),
+-- MATRIMONIO(Nombre 1, Nombre 2, Fecha Realizaci´on, Fecha Fin)
+-- Se asume que dos personas pueden contraer matrimonio entre sı mas de una vez.
+/*
+CREATE TABLE [MATRIMONIO]
+(
+  	[name1] NVARCHAR(160)  NOT NULL,
+  	[name2] NVARCHAR(160)  NOT NULL,
+    [fechaRealizacion] DATE  NOT NULL,    
+    [fechaFin] DATE  NOT NULL,
+    CONSTRAINT [PK_MATRIMONIO] PRIMARY KEY  ([name1], [name2], [fechaRealizacion])
+);
+
+INSERT INTO [MATRIMONIO] ([name1], [name2], [fechaRealizacion], [fechaFin]) VALUES ('Mario','Peach', '2000-01-01', '2001-09-11');
+INSERT INTO [MATRIMONIO] ([name1], [name2], [fechaRealizacion], [fechaFin]) VALUES ('Mario','Peach', '2010-01-01', '2011-09-11');
+*/
+/*
+CREATE TABLE [PERSONA]
+(
+  	[name] NVARCHAR(160)  NOT NULL,
+  	[genero] NVARCHAR(1)  NOT NULL,
+    [fechaBorn] DATE  NOT NULL,
+	[NombreMadre] NVARCHAR(160)  NOT NULL,
+	[NombrePadre] NVARCHAR(160)  NOT NULL
+
+);
+
+INSERT INTO [PERSONA] ([name], [genero], [fechaBorn], [NombreMadre], [NombrePadre]) VALUES ('Toad','X', '2000-02-01', 'Peach', 'Mario');
+INSERT INTO [PERSONA] ([name], [genero], [fechaBorn], [NombreMadre], [NombrePadre]) VALUES ('Blue Toad','X', '2005-02-01', 'Mario', 'Peach');
+INSERT INTO [PERSONA] ([name], [genero], [fechaBorn], [NombreMadre], [NombrePadre]) VALUES ('Bowser Jr.','X', '2000-12-01', 'Browser', 'Big Boo');
+*/
+
+/*
+SELECT DISTINCT p.NombreMadre, p.NombrePadre from PERSONA p
+INNER JOIN MATRIMONIO m on (((m.name1, m.name2) = (p.NombrePadre, p.nombreMadre)) OR ((m.name1, m.name2) = (p.nombreMadre, p.NombrePadre)))
+WHERE p.fechaBorn >= m.fechaRealizacion AND p.fechaBorn <= m.fechaFin
+*/
+
+-- 2.8)
+/*
+Miembro (nombrePersona, nombreGrupo)
+Amigo (nombrePersona1,nombrePersona2)
+Persona (nombrePersona, edad, genero)
+Grupo (nombreGrupo, fechaInicio)
+*/
+--(a) Obtener, en SQL, para cada persona los grupos en los cuales el no es miembro pero todos sus amigos si son miembros. 
+--La consulta debe devolver tuplas con (nombrePersona, nombreGrupo) donde nombreGrupo es el nombre del grupo que cumple 
+--con loque se pide.
+
+
+              
+         
+
 
 
 
